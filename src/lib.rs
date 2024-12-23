8f8e839a74b9ea759a9ab7943860698a3819cde5
vladkvit <vladkvit@outlook.com>
2024-12-23 12:26:26 -0500

    slight tweaks

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 use rayon::prelude::*;
 
 pub fn fib(n: i32) -> i32 {
-    optick::event!();
+    optick::event!(); // This event is NOT captured
     if n <= 0 {
         0
     } else if n == 1 {
@@ -12,13 +12,8 @@ pub fn fib(n: i32) -> i32 {
 }
 
 pub fn multi_fib(nums: &Vec<i32>) -> Vec<i32> {
-    optick::event!();
+    optick::event!(); // This event is captured
+
     // Parallel map each element in `nums` through `fib` and collect into a Vec<i32>
     nums.par_iter().map(|&num| fib(num)).collect()
-}
-
-fn main() {
-    let inputs = vec![5, 10, 15];
-    let results = multi_fib(&inputs);
-    println!("Fibonacci results: {:?}", results);
 }
